536b183185ce2bfa04e236e3bccb9d111deccc62
Shayna Thompson <shaynathompson@users.noreply.github.com>
2019-02-08 17:58:33 +0100

    build folder added

---
+++ build/precache-manifest.3d3b5accf7d4b8ad659f67c3903184a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6ea116aaea706b4d94b",
+    "url": "/static/js/main.a6ea116a.chunk.js"
+  },
+  {
+    "revision": "c24a05a0b3c7aa403c1a",
+    "url": "/static/js/1.c24a05a0.chunk.js"
+  },
+  {
+    "revision": "a6ea116aaea706b4d94b",
+    "url": "/static/css/main.1363e906.chunk.css"
+  },
+  {
+    "revision": "c24a05a0b3c7aa403c1a",
+    "url": "/static/css/1.019ad78f.chunk.css"
+  },
+  {
+    "revision": "955f9168e05a557988a6afe6532c21c7",
+    "url": "/index.html"
+  }
+];
